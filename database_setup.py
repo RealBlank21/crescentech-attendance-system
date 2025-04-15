f7c53c7fd2b5c7272590b90f5da8549eca26be5c
RealBlank21 <realblank21@gmail.com>
2025-04-16 02:24:39 +0800

    remove .env

--- database_setup.py
+++ database_setup.py
@@ -5,8 +5,7 @@ import bcrypt
 load_dotenv()
 
 MYSQL_USERNAME = os.getenv("MYSQL_USERNAME")
-#MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_PASSWORD = ""
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
